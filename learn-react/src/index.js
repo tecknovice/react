9a033a3a91ffdb9b662ff41f5e37abc0510a242a
tecknovice <tecknovice@gmail.com>
2020-08-17 21:39:42 +0700

    add components

--- learn-react/src/index.js
+++ learn-react/src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+// import Toggle from './Toggle';
+import LoginControl from './LoginControl';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <LoginControl />
   </React.StrictMode>,
   document.getElementById('root')
 );
