0c41c35e0f64c3a7cfeadfa20aad850899591921
tecknovice <tecknovice@gmail.com>
2020-08-20 21:37:26 +0700

    LiftingStateUp

--- learn-react/src/index.js
+++ learn-react/src/index.js
@@ -4,12 +4,13 @@ import './index.css';
 // import App from './App';
 // import Toggle from './Toggle';
 // import LoginControl from './LoginControl';
-import Form from "./Form";
+// import Form from "./Form";
+import Calculator from "./LiftingStateUp/Calculator";
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Form />
+    <Calculator />
   </React.StrictMode>,
   document.getElementById('root')
 );
