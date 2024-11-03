136e3d1e7fd3b00450a74c9882998c27c3ef5d71
tecknovice <tecknovice@gmail.com>
2024-11-03 22:05:56 +0900

    update github logo

--- github-ranking/src/components/Navbar.tsx
+++ github-ranking/src/components/Navbar.tsx
@@ -3,7 +3,7 @@ export const Navbar = () => {
     <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark ">
       <div className="container">
         <a className="navbar-brand" href="/">
-          Github ranking
+          Github Ranking
         </a>
       </div>
     </nav>
